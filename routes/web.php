8a95407af6ad0b5e3dd3b310c987744cb438ff59
20_Ishikawa_Kensho <lydin_note@yahoo.co.jp>
2019-07-25 05:03:35 +0000

    PHP/Laravel 09 Routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+public function create(){
+    return redirect('admin/profile/create');
+}
+*/
+/*
+admin/profile/edit にアクセスしたら
+ProfileController の　edit Action に割り当たるように設定する
+*/
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::get('news/create', 'Admin¥NewsController@add');
+    /*------------Task4-start----------------------------------*/
+    Route::get('profile/edit', 'Admin¥ProfileController@edit');
+    /*------------Task4-end------------------------------------*/
+});
+
+/*
+「http://XXXXXX.jp/XXX というアクセスが来たときに、 
+AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください。
+*/
+
+/*------------Task3-start----------------------------------*/
+Route::group(['prefix' => 'XXX'], function(){
+    Route::get('bbb','Admin\AAAController@add');
+});
+/*------------Task3-end----------------------------------*/
