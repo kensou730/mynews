9665a4752f328f4f48ef74354a257c6e186651db
20_Ishikawa_Kensho <lydin_note@yahoo.co.jp>
2019-08-03 01:10:04 +0000

    PHP/Laravel14 Post data

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,13 +5,30 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //追加分
     public function add(){
         return view('admin.profile.create');
     }
-    public function create(){
+    public function create(Request $request){
+        
+        // Validationを使う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $from = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($from['_token']);
+        
+        // データベースに保存する
+        $profile->fill($from);
+        $profile->save();
+        
+
         return redirect('admin/profile/create');
     }
     public function edit(){
