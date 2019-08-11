1fb58022636cd8859751e33af5b52328c6ebd696
20_Ishikawa_Kensho <lydin_note@yahoo.co.jp>
2019-08-11 07:08:52 +0000

    kensuu1234 upd

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <p>課題です！！追加してみます!</p>
+        <p>課題です！！追加してみます!kensuu1234のみ反映テスト</p>
         <hr color="#c0c0c0">
         @if (!is_null($headline))
             <div class="row">
